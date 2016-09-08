7679da31550893e8df1f36519ef9062300b1ec6d
Tram <tramngo2@gmail.com>
2016-09-08 13:59:12 -1000

    done with push

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,8 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+var nameArray = [];
+nameArray.push('Tram','Ngo');
+
+var genericNumberArray = [1, 2, 3, 4, 5];
+genericNumberArray.push(6,7,8,9,10);
